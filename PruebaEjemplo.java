f5b31174bfb3046db1c067ed7f006f373a1aeae7
BrianY002 <104378087+BrianY002@users.noreply.github.com>
2022-04-25 11:02:34 -0500

    Update PruebaEjemplo.java

--- PruebaEjemplo.java
+++ PruebaEjemplo.java
@@ -1,18 +1,8 @@
-/*
 
-Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
-Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
-*/
 package pruebaejemplo;
-/**
-*
-*
-*/
+
 public class PruebaEjemplo {
 
-/**
- * @param args the command line arguments
- */
 public static void main(String[] args) {
     String [] nombres = new String [5]; 
     
